4472860559e4510f8af893d0253a805a7099b6a0
VasJaro-ViVo <vas.ide2022@gmail.com>
2025-03-31 16:15:53 +0300

    WaitGrup

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	// "io"
 	"net/http"
 	// "os"
@@ -16,23 +17,24 @@ type Emploue struct {
 
 
 func main() {
-	t := time.Now()
+
 	vas := &Emploue{"VAS",38}
-	fmt.Println(vas)
-	fmt.Printf("Hello %v.\n", vas.Name)
+	var wg sync.WaitGroup
+	t := time.Now()
+
 	
 	go func ()  {
 		fmt.Printf("Can emploue %s drink bear ? - %v.\t It's age = %v\n", vas.Name, canDrink(&vas.Age), *getAge(*vas))
 		
-		
 	} ()
-	for i := 0; i < 100; i++ {
-		go requestGoogle(i)
-			
+	for i := 0; i < 15; i++ {
+		wg.Add(1)
+		go requestGoogle(i, &wg)
+		
 	}
-	
+	wg.Wait()
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 4)
 	tt := time.Since(t)
 	fmt.Printf("Total time %v\n", tt)
 }
@@ -46,14 +48,15 @@ func getAge(e Emploue) *int{
 	return &e.Age
 }
 
-func requestGoogle(num int) {
+func requestGoogle(num int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	resp, err := http.Get("https://google.com") 
-   if err != nil { 
-         fmt.Println(err) 
-         return
-   } 
-   defer resp.Body.Close()
-   sC := resp.StatusCode 
-   fmt.Printf("Try - %d status - %v \n", num, sC)
+	if err != nil { 
+			fmt.Println(err) 
+			return
+	} 
+	defer resp.Body.Close()
+	sC := resp.StatusCode 
+	fmt.Printf("Try - %d status - %v \n", num, sC)
 //    io.Copy(os.Stdout, resp.Body)
 }
