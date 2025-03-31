2087c75aa37c605596e5e59bbacd4c985a2ba482
VasJaro-ViVo <vas.ide2022@gmail.com>
2025-03-31 17:09:01 +0300

    Stable status need UPD

--- cmd/main.go
+++ cmd/main.go
@@ -10,45 +10,39 @@ import (
 )
 
 
-type Emploue struct {
-	Name string
-	Age int
-}
+// type Emploue struct {
+// 	Name string
+// 	Age int
+// }
+// func canDrink(age *int) bool {
+// 	return *age >=18
+// }
 
+// func getAge(e Emploue) *int{
+// 	return &e.Age
+// }
 
 func main() {
 
-	vas := &Emploue{"VAS",38}
+	// vas := &Emploue{"VAS",38}
+	// go func ()  {
+	// 	fmt.Printf("Can emploue %s drink bear ? - %v.\t It's age = %v\n", vas.Name, canDrink(&vas.Age), *getAge(*vas))
+	// } ()
+
 	var wg sync.WaitGroup
 	t := time.Now()
 
-	
-	go func ()  {
-		fmt.Printf("Can emploue %s drink bear ? - %v.\t It's age = %v\n", vas.Name, canDrink(&vas.Age), *getAge(*vas))
-		
-	} ()
 	for i := 0; i < 15; i++ {
 		wg.Add(1)
-		go requestGoogle(i, &wg)
+		go statusGoogle(i, &wg)
 		
 	}
 	wg.Wait()
-
-	time.Sleep(time.Second * 4)
 	tt := time.Since(t)
 	fmt.Printf("Total time %v\n", tt)
 }
 
-
-func canDrink(age *int) bool {
-	return *age >=18
-}
-
-func getAge(e Emploue) *int{
-	return &e.Age
-}
-
-func requestGoogle(num int, wg *sync.WaitGroup) {
+func statusGoogle(num int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	resp, err := http.Get("https://google.com") 
 	if err != nil { 
