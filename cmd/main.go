baec9e8c3d6f59d2d0a1d56b6086864643431726
VasJaro-ViVo <vas.ide2022@gmail.com>
2025-03-31 15:57:43 +0300

    Try status code

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "io"
+	"net/http"
+	// "os"
+	"time"
+)
 
 
 type Emploue struct {
@@ -10,10 +16,25 @@ type Emploue struct {
 
 
 func main() {
+	t := time.Now()
 	vas := &Emploue{"VAS",38}
 	fmt.Println(vas)
 	fmt.Printf("Hello %v.\n", vas.Name)
-	fmt.Printf("Can emploue %s drink bear ? - %b.\t It's age = %d", vas.Name, canDrink(&vas.Age), *getAge(*vas))
+	
+	go func ()  {
+		fmt.Printf("Can emploue %s drink bear ? - %v.\t It's age = %v\n", vas.Name, canDrink(&vas.Age), *getAge(*vas))
+		
+		
+	} ()
+	for i := 0; i < 100; i++ {
+		go requestGoogle(i)
+			
+	}
+	
+
+	time.Sleep(time.Second * 10)
+	tt := time.Since(t)
+	fmt.Printf("Total time %v\n", tt)
 }
 
 
@@ -25,3 +46,14 @@ func getAge(e Emploue) *int{
 	return &e.Age
 }
 
+func requestGoogle(num int) {
+	resp, err := http.Get("https://google.com") 
+   if err != nil { 
+         fmt.Println(err) 
+         return
+   } 
+   defer resp.Body.Close()
+   sC := resp.StatusCode 
+   fmt.Printf("Try - %d status - %v \n", num, sC)
+//    io.Copy(os.Stdout, resp.Body)
+}
