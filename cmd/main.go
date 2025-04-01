07908d0b3db683b8c899ab9fb724574ad0f1e9f0
VasJaro-PC <vas.ide2022@gmail.com>
2025-04-01 19:00:13 +0300

    Add close ch

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,8 @@ import (
 
 func main() {
 
+	
+	
 	// vas := &Emploue{"VAS",38}
 	// go func ()  {
 	// 	fmt.Printf("Can emploue %s drink bear ? - %v.\t It's age = %v\n", vas.Name, canDrink(&vas.Age), *getAge(*vas))
@@ -31,27 +33,70 @@ func main() {
 
 	var wg sync.WaitGroup
 	t := time.Now()
+	sites := generateSite()
 
-	for i := 0; i < 15; i++ {
-		wg.Add(1)
+
+	for site := range sites {
+		for i := 0; i < 5; i++ {
+			wg.Add(1)
+			go func() {
+				resp := <- statusSite(site.URL)
+				fmt.Println(resp)
+				wg.Done()			
+			}()
+		}
+		
 		go func() {
-			statusGoogle(i)
-			defer wg.Done()
+			wg.Wait()
+			close(sites)
+			
 		}()
+		
+
 	}
-	wg.Wait()
+
 	tt := time.Since(t)
 	fmt.Printf("Total time %v\n", tt)
 }
 
-func statusGoogle(num int) {
-	resp, err := http.Get("https://google.com") 
-	if err != nil { 
+func statusSite(site string) <- chan string {
+		ch := make(chan string, 1)
+		resp, err := http.Get(site) 
+		if err != nil { 
 			fmt.Println(err) 
-			return
-	} 
-	defer resp.Body.Close()
-	sC := resp.StatusCode 
-	fmt.Printf("Try - %d status - %d \n",num, sC)
+			ch <- err.Error()
+			return ch
+				
+		} 
+		defer resp.Body.Close()
+		info := fmt.Sprintf("Try to - %s  status - %d.",site, resp.StatusCode) 
+		ch <- info
+		return ch
 //    io.Copy(os.Stdout, resp.Body)
+}
+
+func generateSite() chan SiteInfo{
+	ch := make(chan SiteInfo)
+	sites := []SiteInfo{
+		SiteInfo{"Distrowatch","https://distrowatch.com/"},
+		SiteInfo{"Vanilla","https://vanillaos.org/"},
+		SiteInfo{"AlmaLinux","https://almalinux.org/"},
+		SiteInfo{"Nobara","https://nobaraproject.org/"},
+		SiteInfo{"POP.os","https://system76.com/pop/"},
+		// SiteInfo{"Fedora","https://fedoraproject.org/"},
+		SiteInfo{"OpenSuse","https://www.opensuse.org/"},
+		// SiteInfo{"Manjaro","https://manjaro.org/"},
+		// SiteInfo{"Debian","https://www.debian.org/"},
+		SiteInfo{"Google","https://google.com"}}
+		go func ()  {
+			for _, v := range sites {
+				ch <- v
+			}	
+		}()
+	return ch
+}
+
+type SiteInfo struct {
+	Name string
+	URL string
 }
